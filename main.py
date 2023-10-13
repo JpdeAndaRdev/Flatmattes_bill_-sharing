dd4d97923df97ed0b9db9ab6ba5f3b3783ee4589
Juan Pablo <jpsilvernova@gmail.com>
2023-10-13 14:57:58 -0600

    First Commit

---
+++ main.py
@@ -0,0 +1,49 @@
+class Bill:
+    """
+    Object that contains data about a bill, such as 
+    total amount and period of the bill.
+    """
+   
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    Creates a flatmate person who lives in the flat
+    and pays a share of the bill.
+    """
+   
+    def __init__(self, name, days_in_house,):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
+
+
+class PdfReport: 
+    """
+    Creates a PDF file that contains data about 
+    the flatmates such as their names, their due
+    amount and the period of the bill.
+    """
+    
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+    
+
+
+the_bill = Bill(amount = 120, period = "March 2022")
+jonh = Flatmate(name = "Jonh", days_in_house = 20)
+marry = Flatmate(name = "Marry", days_in_house = 25)
+
+print("Jonh pays: ", jonh.pays(bill = the_bill, flatmate2 = marry))
+print("Marry pays: ", marry.pays(bill = the_bill, flatmate2 = jonh))
+
