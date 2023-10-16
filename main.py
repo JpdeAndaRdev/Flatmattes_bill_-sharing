6d968aca44a13642cfd85bd61885ceba236fa288
Juan Pablo <jpsilvernova@gmail.com>
2023-10-16 14:03:35 -0600

    Second commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import webbrowser
+from fpdf import FPDF
+
 class Bill:
     """
     Object that contains data about a bill, such as 
@@ -36,14 +39,47 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2),2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add icon
+        pdf.image(name='house.png', w=30, h=30)
+
+        # Insert title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align='C', ln=1)
+
+        # Insert period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Insert name and due amount of the first flatmate
+        pdf.set_font(family='Times', size=12,)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+         # Insert name and due amount of the second flatmate
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
+
+        pdf.output(self.filename)
+
+        webbrowser.open(self.filename)
+
     
 
-
-the_bill = Bill(amount = 120, period = "March 2022")
+the_bill = Bill(amount = 120, period = "April 2022")
 jonh = Flatmate(name = "Jonh", days_in_house = 20)
 marry = Flatmate(name = "Marry", days_in_house = 25)
 
 print("Jonh pays: ", jonh.pays(bill = the_bill, flatmate2 = marry))
 print("Marry pays: ", marry.pays(bill = the_bill, flatmate2 = jonh))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatmate1=jonh, flatmate2=marry, bill=the_bill)
 
