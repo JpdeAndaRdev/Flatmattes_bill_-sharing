22d7f27cafa2a4bf151ab6581b9a8dba5c2b0ffc
Juan Pablo <jpsilvernova@gmail.com>
2023-10-26 10:27:00 -0600

    bill_form_page_commit

--- main.py
+++ main.py
@@ -17,6 +17,20 @@ class BillFormPage(MethodView):
         bill_form = BillForm()
         return render_template('bill_form_page.html', billform=bill_form)
     
+    def post(self):
+        billform = BillForm(request.form)
+
+        the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+        flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+        flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+
+        return render_template('bill_form_page.html',
+                               result = True,
+                               billform = billform,
+                               name1 = flatmate1.name,
+                               amount1 = flatmate1.pays(the_bill, flatmate2),
+                               name2 = flatmate2.name,
+                               amount2 = flatmate2.pays(the_bill, flatmate1))
     
 class ResultsPage(MethodView):
     
@@ -49,8 +63,8 @@ class BillForm(Form):
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
-app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
+app.add_url_rule('/bill_form_page', view_func=BillFormPage.as_view('bill_form_page'))
+# app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
 
 app.run(debug=True)
 
